5b5acd4577a69c0d2dea4df3f825977161417564
zy_zero <jiyoungjang0307@gmail.com>
2023-11-03 00:56:40 +0900

    whileì과 단축 연산자를 이용í하여 각 자릿수의 합 구하기

---
+++ src/part03/WhileContraction.java
@@ -0,0 +1,18 @@
+package part03;
+
+public class WhileContraction {
+    public static int sumEach(int n) {
+        int tot = 0;
+        while (n != 0) {
+            tot += n % 10;   // 3 -> 2 -> 1
+            n /= 10;         // 123 -> 12 -> 1 -> 0
+        }
+        return tot;
+    }
+
+    public static void main(String[] args) {
+        int number = 1234567;
+        int value = sumEach(number);
+        System.out.printf("%d에 대한 각 자리의 숫자 합 : %d", number, value);
+    }
+}
