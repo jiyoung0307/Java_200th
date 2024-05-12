9ee87862df23d6f6692c1ecb8623b46a7838c395
zy_zero <jiyoungjang0307@gmail.com>
2024-05-12 14:59:20 +0900

    intNums를 scanner로 변경

--- src/part03/BitNShift/BitNShiftMainTwo.java
+++ src/part03/BitNShift/BitNShiftMainTwo.java
@@ -1,5 +1,7 @@
 package part03.BitNShift;
 
+import java.util.Scanner;
+
 /* 문자열 자르기를 이요하여 2진수 문자열로 바꾸기 */
 public class BitNShiftMainTwo {
     /* 10진법수를 2진수 스프링으로 변환 */
@@ -16,9 +18,18 @@ public class BitNShiftMainTwo {
     }
 
     public static void main(String[] args) {
-        int intNums1 = 123;
-        int intNums2 = -123;
+//        int intNums1 = 123;
+//        int intNums2 = -123;
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("intNums1을 입력하세요 : ");
+        int intNums1 = sc1.nextInt();
         System.out.printf("%d : %s%n", intNums1, shifts(intNums1));
+
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("intNums2를 입력하세요 : ");
+        int intNums2 = sc2.nextInt();
         System.out.printf("%d : %s%n", intNums2, shifts(intNums2));
     }
 }
